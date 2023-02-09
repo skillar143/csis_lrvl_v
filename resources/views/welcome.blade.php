1e2d7d9cc67b198db56d395bf4c5c50f0d6761d9
skillar143 <jeormelagancia@gmail.com>
2023-02-10 02:16:19 +0800

    landing-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,44 +20,17 @@
         <!-- Background Video-->
         <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="{{ asset('/img/bg.mp4') }}" type="video/mp4" /></video>
         <!-- Masthead-->
+
         <div class="masthead">
             <div class="masthead-content text-white">
                 <div class="container-fluid px-4 px-lg-0">
                     <h1 class="fst-italic lh-1 mb-4">Canossa San Pablo</h1>
-                    <p class="mb-5">O God, Father of goodness, you revealed your love to the humble and to the children, by raising in your Church, St. Magdalene of Canossa to be the servant of the poor, grant that we may seek you above all things and served the poor and the little ones in a true spirit of charity and humility. Through Christ our Lord. Amen.</p>
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- * * SB Forms Contact Form * *-->
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- This form is pre-integrated with SB Forms.-->
-                    <!-- To make this form functional, sign up at-->
-                    <!-- https://startbootstrap.com/solution/contact-forms-->
-                    <!-- to get an API token!-->
-                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                        <!-- Email address input-->
+                    <p class="mb-5">"Be good, love the Lord, pray for those who do not know Him. What a great grace it is to know God."</p>
+
                         <div class="row input-group-newsletter">
-                            <div class="col"><input class="form-control" id="email" type="email" placeholder="Enter email address..." aria-label="Enter email address..." data-sb-validations="required,email" /></div>
-                            <div class="col-auto"><button class="btn btn-primary disabled" id="submitButton" type="submit">Notify Me!</button></div>
+
+                            <div class="col-auto"><a class="btn btn-info btn-lg log"  href="{{ route('login') }}">Log in</a></div>
                         </div>
-                        <div class="invalid-feedback mt-2" data-sb-feedback="email:required">An email is required.</div>
-                        <div class="invalid-feedback mt-2" data-sb-feedback="email:email">Email is not valid.</div>
-                        <!-- Submit success message-->
-                        <!---->
-                        <!-- This is what your users will see when the form-->
-                        <!-- has successfully submitted-->
-                        <div class="d-none" id="submitSuccessMessage">
-                            <div class="text-center mb-3 mt-2">
-                                <div class="fw-bolder">Form submission successful!</div>
-                                To activate this form, sign up at
-                                <br />
-                                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                            </div>
-                        </div>
-                        <!-- Submit error message-->
-                        <!---->
-                        <!-- This is what your users will see when there is-->
-                        <!-- an error submitting the form-->
-                        <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3 mt-2">Error sending message!</div></div>
-                    </form>
                 </div>
             </div>
         </div>
@@ -65,9 +38,9 @@
         <!-- For more icon options, visit https://fontawesome.com/icons?d=gallery&p=2&s=brands-->
         <div class="social-icons">
             <div class="d-flex flex-row flex-lg-column justify-content-center align-items-center h-100 mt-3 mt-lg-0">
-                <a class="btn btn-dark m-3" href="#!"><i class="fab fa-twitter"></i></a>
-                <a class="btn btn-dark m-3" href="#!"><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark m-3" href="#!"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-dark m-3" href="https://twitter.com/CanossaSPC"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark m-3" href="https://www.facebook.com/canossacollegespc/"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark m-3" href="https://www.instagram.com/explore/tags/canossasanpablo/"><i class="fab fa-instagram"></i></a>
             </div>
         </div>
         <!-- Bootstrap core JS-->
