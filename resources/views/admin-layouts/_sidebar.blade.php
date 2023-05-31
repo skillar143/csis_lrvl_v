24262f038973c089c227224be11e3fb595bdac20
skillar143 <jeormlagancia@gmail.com>
2023-06-01 06:57:13 +0800

    sidebar navigation help

--- resources/views/admin-layouts/_sidebar.blade.php
+++ resources/views/admin-layouts/_sidebar.blade.php
@@ -12,8 +12,8 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('dashboard') }}">
+    <li class="nav-item {{ preg_match('/\bdashboard\b/i', request()->fullUrl()) ? 'bg-light' : '' }}">
+        <a class="nav-link {{ preg_match('/\bdashboard\b/i', request()->fullUrl()) ? 'text-dark' : '' }}" href="{{ route('dashboard') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -28,22 +28,25 @@
 
     <!-- Nav Item - Student Collapse Menu-->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStudent"
+        <a class="nav-link collapsed " href="#" data-toggle="collapse" data-target="#collapseStudent"
             aria-expanded="true" aria-controls="collapseStudent">
             <i class="fas fa-user"></i>
             <span>Student</span>
         </a>
-        <div id="collapseStudent" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseStudent" class="collapse {{ (strpos(request()->fullUrl(), 'Student') !== false)  ? 'show' : '' }}"
+            aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Type of student:</h6>
-                <a class="collapse-item" href="{{ route('student.index','0') }}">Regular Student</a>
-                <a class="collapse-item" href="{{ route('student.index','1') }}">Irregular Student</a>
+                <a class="collapse-item {{  preg_match('/^(?=.*\bStudent\b)(?=.*\b0\b).*$/', request()->fullUrl()) ? 'bg-dark text-light' : '' }}"
+                    href="{{ route('student.index','0') }}">Regular Student</a>
+                <a class="collapse-item {{  preg_match('/^(?=.*\bStudent\b)(?=.*\b1\b).*$/', request()->fullUrl()) ? 'bg-dark text-light' : '' }}"
+                    href="{{ route('student.index','1') }}">Irregular Student</a>
             </div>
         </div>
     </li>
     <!-- Nav Item - Faculty -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.faculty') }}">
+    <li class="nav-item {{ (strpos(request()->fullUrl(), 'Faculty') !== false)  ? 'bg-light' : '' }}">
+        <a class="nav-link {{ (strpos(request()->fullUrl(), 'Faculty') !== false)  ? 'text-dark' : '' }}" href="{{ route('admin.faculty') }}">
             <i class="fas fa-user-tie"></i>
             <span>Faculty</span></a>
     </li>
@@ -55,11 +58,13 @@
             <i class="fas fa-book"></i>
             <span>Program</span>
         </a>
-        <div id="collapseProgram" class="collapse" aria-labelledby="headingUtilities"
+        <div id="collapseProgram" class="collapse {{ preg_match('/^(?=.*\bProgram\b)|(?=.*\bCurriculum\b).*$/', request()->fullUrl()) ? 'show' : '' }}" aria-labelledby="headingUtilities"
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="{{ route('subject.index') }}">Subject</a>
-                <a class="collapse-item" href="{{ route('course.index') }}">Course</a>
+                <a class="collapse-item {{  preg_match('/^(?=.*\bProgram\b)(?=.*\bSubject\b).*$/', request()->fullUrl()) ? 'bg-dark text-light' : '' }}"
+                    href="{{ route('subject.index') }}">Subject</a>
+                <a class="collapse-item {{  preg_match('/^(?=.*\bProgram\b)(?=.*\bCourse\b)|(?=.*\bCurriculum\b).*$/', request()->fullUrl()) ? 'bg-dark text-light' : '' }}"
+                    href="{{ route('course.index') }}">Course</a>
             </div>
         </div>
     </li>
@@ -80,21 +85,21 @@
     </li>
 
     <!-- Nav Item - Grade Summary -->
-    <li class="nav-item">
-        <a class="nav-link" href="#">
+    <li class="nav-item {{ preg_match('/\bGradeSummary\b/i', request()->fullUrl()) ? 'bg-light' : '' }}">
+        <a class="nav-link {{ preg_match('/\bGradeSummary\b/i', request()->fullUrl()) ? 'text-dark' : '' }}" href="#">
             <i class="fas fa-award"></i>
             <span>Grade Summary</span></a>
     </li>
     <!-- Nav Item - Grade Sheet -->
-    <li class="nav-item">
-        <a class="nav-link" href="#">
+    <li class="nav-item {{ preg_match('/\bGradeSheet\b/i', request()->fullUrl()) ? 'bg-light' : '' }}">
+        <a class="nav-link {{ preg_match('/\bGradeSheet\b/i', request()->fullUrl()) ? 'text-dark' : '' }}" href="#">
             <i class="fas fa-list-alt"></i>
             <span>Grading Sheet</span></a>
     </li>
 
      <!-- Nav Item - Assess Grade -->
-     <li class="nav-item">
-        <a class="nav-link" href="#">
+     <li class="nav-item {{ preg_match('/\bAssessGrade\b/i', request()->fullUrl()) ? 'bg-light' : '' }}">
+        <a class="nav-link {{ preg_match('/\bAssessGrade\b/i', request()->fullUrl()) ? 'text-dark' : '' }}" href="#">
             <i class="fas fa-id-card"></i>
             <span>Assess Grade</span></a>
     </li>
@@ -132,12 +137,15 @@
             <i class="fas fa-users-slash"></i>
             <span>Users</span>
         </a>
-        <div id="collapseUser" class="collapse" aria-labelledby="headingUtilities"
+        <div id="collapseUser" class="collapse {{ preg_match('/\buser\b/i', request()->fullUrl()) ? 'show' : '' }}" aria-labelledby="headingUtilities"
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Type of User:</h6>
-                <a class="collapse-item" href="{{ route('users.view','student') }}">Student</a>
-                <a class="collapse-item" href="{{ route('users.view','faculty') }}">Faculty</a>
+                <a class="collapse-item {{ (strpos(request()->fullUrl(), 'user') !== false) && (strpos(request()->fullUrl(), 'student') !== false)  ? 'bg-dark text-light' : '' }}"
+                    href="{{ route('users.view','student') }}">Student</a>
+
+                <a class="collapse-item {{ (strpos(request()->fullUrl(), 'user') !== false) && (strpos(request()->fullUrl(), 'faculty') !== false)  ? 'bg-dark text-light' : '' }}"
+                     href="{{ route('users.view','faculty') }}">Faculty</a>
             </div>
         </div>
     </li>
